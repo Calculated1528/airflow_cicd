bcff7465283490fd3ba65d5fbfb09c07ba3e04d6
calculated1528 <calculated1528@gmail.com>
2024-09-04 22:39:39 +0500

    rewrite_ci_file

--- .github/workflows/update-staging.sh
+++ .github/workflows/update-staging.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
-cd /opt/dags/airflow_dags
-
-git pull
-
-cp -r /opt/dags/airflow_dags/* /opt/airflow/dags/
-
-git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$()/" > /opt/airflow/dags/.version
-git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/" >> /opt/airflow/dags/.version
-exit
-
+name: 💿 CI Staging
+on:
+  push:
+    branches:
+      - "staging"
+jobs:
+  build-dev:
+    name: 🏗 Update staging from server
+    runs-on: self-hosted
+    env:
+      CONTEXT: .
+    steps:
+      - name: 🎉 Update staging
+        uses: appleboy/ssh-action@master
+        with:
+          host: ${{ secrets.CI_AIRFLOW_HOST }}
+          username: ${{ secrets.CI_AIRFLOW_USER }}
+          key: ${{ secrets.CI_AIRFLOW_KEY }}
+          script: sh /opt/dags/update-staging.sh
